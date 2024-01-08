bc1722a943d8b04928e93b95dc7e0223d3116129
Lerian Febriana, A.Md.Kom <kanglerian@gmail.com>
2024-01-08 16:34:02 +0700

    first

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const fs = require('fs');
 const path = require('path');
+const cors = require('cors');
 const app = express();
 const port = 3032;
 
+app.use(cors());
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ limit: '10mb', extended: true }));
 app.use(express.static(path.join(__dirname, 'uploads')));
