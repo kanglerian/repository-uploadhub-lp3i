a2e719e93ae5dc768c124fe3c599e0c6523756c5
Lerian Febriana, A.Md.Kom <kanglerian@gmail.com>
2024-01-30 14:43:38 +0700

    sip

--- index.js
+++ index.js
@@ -6,8 +6,8 @@ const app = express();
 const port = 3032;
 
 app.use(cors());
-app.use(express.json({ limit: '20mb' }));
-app.use(express.urlencoded({ limit: '20mb', extended: true }));
+app.use(express.json({ limit: '100mb' }));
+app.use(express.urlencoded({ limit: '100mb', extended: true }));
 app.use(express.static(path.join(__dirname, 'uploads')));
 
 app.get('/', (req, res) => {
